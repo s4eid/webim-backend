35219afcd94c603bf794078a2062f0988e5e899c
saeid <saeid.noormohammad@gmail.com>
2021-10-07 19:03:21 +0300

    here we go

--- db/postgresConnection.js
+++ db/postgresConnection.js
@@ -1,14 +1,7 @@
 const { Pool } = require("pg");
-const password = process.env.PASSWORD;
-console.log(password);
 const pool = new Pool({
-  user: process.env.USER,
-  host: process.env.HOST,
-  database: process.env.DATABASE,
-  password: process.env.PASSWORD,
   connectionString: process.env.DATABASE_URL,
   ssl: { rejectUnauthorized: false },
-  port: process.env.PORTT,
 });
 
 module.exports = { pool };
--- graphql/projects/resolvers/projects_r.js
+++ graphql/projects/resolvers/projects_r.js
@@ -3,7 +3,6 @@ const projectsResolver = {
     async projects(parent, args, { pool }) {
       try {
         const data = await pool.query("SELECT * FROM projects");
-        console.log(data.rows);
         return data.rows;
       } catch (error) {
         console.log(error);
